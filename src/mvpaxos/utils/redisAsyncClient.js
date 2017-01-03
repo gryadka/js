263ce2b8d5ef5be67c6448fbc2860399f024cccb
Denis Rystsov <rystsov.denis@gmail.com>
2017-01-03 00:04:17 -0800

    adds ability to reconnect to a redis node if the connection fails

--- src/mvpaxos/utils/redisAsyncClient.js
+++ src/mvpaxos/utils/redisAsyncClient.js
@@ -4,5 +4,5 @@ import Promise from "bluebird";
 Promise.promisifyAll(redis.RedisClient.prototype);
 
 export default function redisAsyncClient(port, host) {
-    return redis.createClient(port, host);
+    return redis.createClient({port: port, host: host, retry_strategy: options=>2000});
 }

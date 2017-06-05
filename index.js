cd01db9802e80a0b662b57bfdb66b8fb28f98388
Denis Rystsov <rystsov.denis@gmail.com>
2017-06-04 20:21:16 -0700

    Exposes Time API to write Acceptors mocks

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const {Cache} = require("./src/Cache");
 const {AcceptorClient} = require("./src/AcceptorClient");
 const {Proposer} = require("./src/Proposer");
+const {Tick} = require("./src/Tick");
 
 exports.Cache = Cache;
 exports.AcceptorClient = AcceptorClient;
 exports.Proposer = Proposer;
+exports.Tick;
